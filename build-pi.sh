03874c758d2c2487afc1612bc0f638bfb3250bcc
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2017-09-01 23:49:10 +0200

    Technique: build raspi cross compiled only. Remove clang

--- build-pi.sh
+++ build-pi.sh
@@ -1,54 +1,24 @@
 #!/usr/bin/env bash
 # Author : Gregory DEPUILLE
 # Description : Wrapper pour la configuration et le build du projet
-PROJECT="ARIG C++ RPLidar Bridge (cross compile Raspberry PI ou remote build)"
-
-BUILD_MODE=${1:-"local"}
-ROBOT=${2:-"nerell"}
+PROJECT="ARIG C++ RPLidar Bridge (cross compile Raspberry PI)"
 
 echo "Build du projet $PROJECT"
-echo " - Build local : ./build-pi.sh [local]"
-echo " - Build remote : ./build-pi.sh remote [nerell]"
-echo ""
 
-if [[ "${BUILD_MODE}" == "local" ]] ; then
-    ROOT_DIR=$(pwd)
-    BUILD_DIR=$ROOT_DIR/build-pi
+ROOT_DIR=$(pwd)
+BUILD_DIR=$ROOT_DIR/build-pi
 
-    if [ -d "$BUILD_DIR" ] ; then
-        echo "-- Nettoyage du répertoire de build $BUILD_DIR"
-        rm -Rf $BUILD_DIR
-    fi
-
-    echo "-- Création du répertoire de build $BUILD_DIR"
-    mkdir -p $BUILD_DIR
-
-    echo "-- Build du projet $PROJECT"
-    cd $BUILD_DIR
-    export PI_TOOLS_HOME=$(pwd)/../download/tools
-    cmake -DCMAKE_TOOLCHAIN_FILE=raspberry.cmake .. || exit $?
-    cmake --build . || exit $?
-    echo "Build terminé"
-
-elif [[ "${BUILD_MODE}" == "remote" ]] ; then
-    DEST_DIR=/home/pi/rplidarBridge
-
-    echo "-- SSH : Création répertoire RPLidar Bridge"
-    ssh $ROBOT mkdir -p $DEST_DIR
-
-    echo "-- SSH : Nettoyage des sources"
-    ssh $ROBOT rm -Rvf $DEST_DIR/src
-
-    echo "-- SSH : Copie des sources sur le Raspberry $ROBOT";
-    scp build.sh $ROBOT:$DEST_DIR/
-    scp download.sh $ROBOT:$DEST_DIR/
-    scp CMakeLists.txt $ROBOT:$DEST_DIR/
-    scp -r src $ROBOT:$DEST_DIR/
-
-    echo "-- SSH : Build sur le Raspberry $ROBOT"
-    ssh $ROBOT "cd $DEST_DIR ; sh build.sh"
-
-    echo "-- SSH : Build terminé"
-else
-    echo "Build mode [$BUILD_MODE] non supporté. Seulement local et remote."
+if [ -d "$BUILD_DIR" ] ; then
+    echo "-- Nettoyage du répertoire de build $BUILD_DIR"
+    rm -Rf $BUILD_DIR
 fi
+
+echo "-- Création du répertoire de build $BUILD_DIR"
+mkdir -p $BUILD_DIR
+
+echo "-- Build du projet $PROJECT"
+cd $BUILD_DIR
+export PI_TOOLS_HOME=$(pwd)/../download/tools
+cmake -DCMAKE_TOOLCHAIN_FILE=raspberry.cmake .. || exit $?
+cmake --build . || exit $?
+echo "Build terminé"
